bb4d79ec628a3577724e43fab66140d40b55bb77
Stefan Wiehler <me@sephalon.net>
2019-01-26 18:26:18 +0100

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
-#!/usr/bin/env python
+import setuptools
 
-from distutils.core import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-setup(
+setuptools.setup(
     name='Pyjector',
     version='0.2.3',
-    description='Control your projector over a serial port',
     author='John Brodie',
     author_email='john@brodie.me',
+    description='Control your projector over a serial port',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='http://www.github.com/JohnBrodie/pyjector',
-    packages=['pyjector'],
-    install_requires=[
-        'pyserial',
-    ],
+    packages=setuptools.find_packages(),
+    install_requires=['pyserial'],
     package_data={
         'pyjector': ['projector_configs/*.json'],
     },
